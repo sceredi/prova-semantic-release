cdb51b44406e7c29bfde406cb20d78d3bbe08ed6
Luca Fabri <luca.fabri1999@gmail.com>
2024-07-27 14:53:52 +0200

    chore!: trying trigger release

--- release.config.js
+++ release.config.js
@@ -1,16 +1,6 @@
-const config = {
-  branches: ['main'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    ["@semantic-release/git", {
-      "assets": ["dist/*.js", "dist/*.js.map"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
-    '@semantic-release/changelog',   // Updates the changelog file with release notes
-    '@semantic-release/github',  // Publishes the release on GitHub
-    '@semantic-release/git'  // Commits the changelog and package.json version changes
-  ]
-};
-
-module.exports = config;
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    "@semantic-release/github",
+    "@semantic-release/git",
+)
+module.exports = config
