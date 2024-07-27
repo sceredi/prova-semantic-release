70147ababd2dad5b4c87ec7562088f92e0db65a3
Luca Fabri <luca.fabri1999@gmail.com>
2024-07-27 14:40:39 +0200

    chore(release): 1.0.0

--- release.config.js
+++ release.config.js
@@ -7,7 +7,9 @@ const config = {
       "assets": ["dist/*.js", "dist/*.js.map"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    '@semantic-release/github'
+    '@semantic-release/changelog',   // Updates the changelog file with release notes
+    '@semantic-release/github',  // Publishes the release on GitHub
+    '@semantic-release/git'  // Commits the changelog and package.json version changes
   ]
 };
 
